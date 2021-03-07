49a0fec4bd8861b0bc425a9ec58634ee54d8e38a
Jiri Gargas <gargas.jiri@gmail.com>
2021-03-07 20:27:48 +0100

    add auth

---
+++ ImmortalFighters.WebApp/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImmortalFighters.WebApp.Middlewares
+{
+    public class AuthenticationMiddleware
+    {
+    }
+}
