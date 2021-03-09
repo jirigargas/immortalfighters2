567dd2bc6152fbc2d3f52b5686971e21b21d2346
Jiri Gargas <gargas.jiri@gmail.com>
2021-03-09 19:28:05 +0100

    add auth module

--- ImmortalFighters.WebApp/ClientApp/src/app/app-routing.module.ts
+++ ImmortalFighters.WebApp/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) },
+  { path: '', redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
