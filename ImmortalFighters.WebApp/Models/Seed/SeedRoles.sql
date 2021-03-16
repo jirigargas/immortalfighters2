7e89b72b5d69c3a1015ccbd77838d25569fe7769
Jiri Gargas <gargas.jiri@gmail.com>
2021-03-16 19:48:06 +0100

    add seed roles script

---
+++ ImmortalFighters.WebApp/Models/Seed/SeedRoles.sql
@@ -0,0 +1,7 @@
+﻿insert into Roles values
+('Admin', 'administrátor'),
+('Patron', 'uživatel s placeným účtem'),
+('Moderátor', 'administrátor fór'),
+('Ekonom', 'administrátor obchodů'),
+('Dozorce', 'administrátor dobrodružství'),
+('VIP', 'čestná role')
